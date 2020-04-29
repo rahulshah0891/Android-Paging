b4d3bbd349bac20923c7e4ab08344fa66a1dd8eb
rahulshah0891 <rahulshah0891@gmail.com>
2020-04-29 13:09:43 +0530

    Removed unwanted code

--- app/src/main/java/com/pagingdemo/ui/main/MainActivity.kt
+++ app/src/main/java/com/pagingdemo/ui/main/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
                 layoutManager = LinearLayoutManager(this@MainActivity)
                 adapter = userAdapter
             }
-            userRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
         }
 
         initObservers()
