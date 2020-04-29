e7423f0a359847bf9dc0e4605cc70ee2e697c72a
rahulshah0891 <rahulshah0891@gmail.com>
2020-04-29 13:19:57 +0530

    Removed unwanted code

--- app/src/main/java/com/pagingdemo/utils/NetworkUtils.kt
+++ app/src/main/java/com/pagingdemo/utils/NetworkUtils.kt
@@ -14,40 +14,6 @@ import androidx.lifecycle.MutableLiveData
  */
 object NetworkUtils {
 
-    private val networkLiveData: MutableLiveData<Boolean> = MutableLiveData()
-
-    /**
-     * Returns instance of [LiveData] which can be observed for network changes.
-     */
-    fun getNetworkLiveData(context: Context): LiveData<Boolean> {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        val networkCallback = object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network?) {
-                networkLiveData.postValue(true)
-            }
-
-            override fun onLost(network: Network?) {
-                networkLiveData.postValue(false)
-            }
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            connectivityManager.registerDefaultNetworkCallback(networkCallback)
-        } else {
-            val builder = NetworkRequest.Builder()
-            connectivityManager.registerNetworkCallback(builder.build(), networkCallback)
-        }
-
-        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
-        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
-
-        networkLiveData.postValue(isConnected)
-
-        return networkLiveData
-    }
-
     fun isOnline(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val netInfo = cm.activeNetworkInfo
